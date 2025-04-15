da16ef1e296d8f36a7b3868089fbd9152ba523ef
Katka Horáková <katka.horakova12@seznam.cz>
2025-04-15 20:01:28 +0200

    add a new output

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        System.Console.WriteLine("preparing to say Hello");
+
+        
     }
 }
